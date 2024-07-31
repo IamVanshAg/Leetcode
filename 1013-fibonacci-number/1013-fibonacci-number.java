699154d142f67b2d5b5833906030fc5445aa5000
Vansh Agarwal <141498818+IamVanshAg@users.noreply.github.com>
2024-07-31 22:32:33 +0530

    Time: 8 ms (51.17%), Space: 40.1 MB (72.05%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,7 +1,7 @@
 class Solution {
     public int fib(int n) {
-        double sqrt5 = Math.sqrt(5);
-        double fibN = Math.pow((1 + sqrt5) / 2, n) - Math.pow((1 - sqrt5) / 2, n);
-        return (int) Math.round(fibN / sqrt5);
+       if(n<=1){
+        return n;
+       }return fib(n-1) + fib(n-2);
     }
 }
