d079c017e993f09b06b1946f6837547cd008fd74
Vansh Agarwal <141498818+IamVanshAg@users.noreply.github.com>
2024-09-07 16:21:33 +0530

    Time: 0 ms (100%), Space: 40.6 MB (77.59%) - LeetHub

--- 0405-convert-a-number-to-hexadecimal/0405-convert-a-number-to-hexadecimal.java
+++ 0405-convert-a-number-to-hexadecimal/0405-convert-a-number-to-hexadecimal.java
@@ -1,18 +1,29 @@
 class Solution {
     public String toHex(int num) {
-        String s = Integer.toString(num, 16);
-        if(num==-1) return "ffffffff";
-        if(num==-2) return "fffffffe";
-        if(num==-3) return "fffffffd";
-        if(num==-4) return "fffffffc";
-        if(num==-5) return "fffffffb";
-        if(num==-6) return "fffffffa";
-        if(num==-10) return "fffffff6";
-        if(num==-100000) return "fffe7960";
-        if(num==-10000000) return "ff676980";
-        if(num==-18899) return "ffffb62d";
-        if(num==-111110) return "fffe4dfa";
-        if(num==-2147483648) return "80000000";
-        return s;
+        String hexString = Integer.toHexString(num);
+        return hexString;
+        // if(num>=0){
+        //     String s = Integer.toString(num, 16);
+        //     return s;
+        // }else{
+        //     int oc = ~num;
+        //     int tc = oc + 1;
+        //     String s1 = Integer.toBinaryString(tc);
+        //     int dV = Integer.parseInt(s1, 2); 
+        //     String hS = Integer.toHexString(dV);
+        //     return hS;
+        // }
+        // if(num==-1) return "ffffffff";
+        // if(num==-2) return "fffffffe";
+        // if(num==-3) return "fffffffd";
+        // if(num==-4) return "fffffffc";
+        // if(num==-5) return "fffffffb";
+        // if(num==-6) return "fffffffa";
+        // if(num==-10) return "fffffff6";
+        // if(num==-100000) return "fffe7960";
+        // if(num==-10000000) return "ff676980";
+        // if(num==-18899) return "ffffb62d";
+        // if(num==-111110) return "fffe4dfa";
+        // if(num==-2147483648) return "80000000";
     }
 }
